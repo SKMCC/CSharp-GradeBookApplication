777ae5f42311bc1ff31f0f62e14d1b95c7c45c77
SeanMcC <seanmccausland@live.co.uk>
2020-05-02 20:06:39 +0100

    Added GradeBookType.cs under Emun Directory
    Module 1 of Pluralsight Interactive Course.

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,13 @@
+namespace GradeBook.Enums
+{
+    public enum GradebookType
+    {
+        Standard,
+        Ranked,
+        ESNU,
+        OneToFour,
+        SixPoint,
+    }
+}
+
+//radeBookType containing the types "Standard", "Ranked", "ESNU", "OneToFour", and "SixPoint".
